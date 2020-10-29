a553f22dea81d59094a67ee6ed61076230894a71
rodrigo <rodrigo@foodbam.com>
2020-10-29 01:06:20 -0500

    Updates

---
+++ precache-manifest.2d9c33da3fb3afa31b6a7eeb1014c400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34acb489cff868c360700ab3801e391",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "aa5910879386a367ce3d",
+    "url": "/react-movies/static/css/main.fb55dae8.chunk.css"
+  },
+  {
+    "revision": "978859dafc062840b0be",
+    "url": "/react-movies/static/js/2.10736ca9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-movies/static/js/2.10736ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5910879386a367ce3d",
+    "url": "/react-movies/static/js/main.c9baf3ac.chunk.js"
+  },
+  {
+    "revision": "67513ede745015a201e5",
+    "url": "/react-movies/static/js/runtime-main.f2ea0420.js"
+  }
+]);
